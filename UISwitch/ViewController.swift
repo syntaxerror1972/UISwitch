5b962a9b131ea689d31a43daf486ef0d1003b349
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-21 01:17:55 +0530

    You use the UISwitch class to create and manage the On/Off buttons used, for example, in the Settings app for options such as Airplane Mode and Bluetooth. These objects are known as switches.

--- UISwitch/ViewController.swift
+++ UISwitch/ViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var switchStatus = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        switchStatus.frame = CGRect(x: 100, y: 200, width: 200, height: 20)
+        view.addSubview(switchStatus)
+        switchStatus.text = "UISwitch is ON"
+        
+        var switchView=UISwitch(frame:CGRect(x: 150, y: 300, width: 0, height: 0));
+        switchView.isOn = true
+        switchView.setOn(true, animated: false);
+        switchView.addTarget(self, action: #selector(switchIsChanged), for: UIControlEvents.valueChanged)
+        self.view.addSubview(switchView);
     }
+    
+    func switchIsChanged(mySwitch: UISwitch) {
+        if mySwitch.isOn {
+            switchStatus.text = "UISwitch is ON"
+        } else {
+            switchStatus.text = "UISwitch is OFF"
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
